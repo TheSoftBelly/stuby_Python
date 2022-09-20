c9ee5c7e504c15ab307d9a0b67a983c533db34bd
Joon <jjoon102426@gmail.com>
2022-09-20 19:31:40 +0900

    try add main_texts_contents

--- crawling_real/make_html_thebell.py
+++ crawling_real/make_html_thebell.py
@@ -13,6 +13,22 @@ for asideBox_href in asideBox_call_hrefs:
 for asideBox_title in asideBox:
     asideBox_titles=asideBox_title.text.strip()
     asideBox_all_titles.append(asideBox_titles)
+# 첫페이지 신규 내용 크롤링
+BASE_LIST_URL = 'https://www.thebell.co.kr/free/content/Article.asp?svccode=00'
+res = requests.get(BASE_LIST_URL)
+soup = BeautifulSoup(res.text , 'html.parser')
+main_texts_titles = soup.select('#contents > div.contentSection > div > div.newsBox > div.newsList > div.listBox > ul > li > dl > a > dt')
+main_texts_contents = soup.select('#contents > div.contentSection > div > div.newsBox > div.newsList > div.listBox > ul > li > dl > a > dd')
+main_texts_reporters = soup.select('#contents > div.contentSection > div > div.newsBox > div.newsList > div.listBox > ul > li > dl > dd > span.user')
+main_texts_titles_list=[]
+main_texts_contents_list=[]
+main_texts_reporters_list=[]
+for main_text in main_texts_titles:
+    main_texts_titles_list.append(main_text.text)
+for main_texts_content in main_texts_contents:
+    main_texts_contents_list.append(main_texts_content.text)
+for main_texts_reporter in main_texts_reporters:
+    main_texts_reporters_list.append(main_texts_reporter.text.strip().replace('\xa0',' '))
 
 
 #날씨 크롤링
@@ -44,20 +60,21 @@ for weather_tems in weather_box:
     for weather_today_tem in weather_today_tems[0:3]:
         weather_today_temlist.append(weather_today_tem.text.strip())
 
+asideBox_ul=''
 #html로 바꾸기
-import os
-print(os.path.isfile("html_test.html"))
+for asideBox_num_for_ul in range(len(asideBox_hrefs)):
+    asideBox_ul = asideBox_ul+'<ul><a href="{asideBox_href}" style="color: black; font-size: initial;">{rank}.  {asideBox_all_title}</a></ul>'.format(asideBox_href=asideBox_hrefs[asideBox_num_for_ul],asideBox_all_title=asideBox_all_titles[asideBox_num_for_ul],rank=asideBox_num_for_ul+1)
+
 
 file = open('html_test.html','w',encoding='UTF-8')
-
-file.write(f'''<div style="background-color: white;padding-bottom: 15%;">
+file_contents=f'''<div style="background-color: white;padding-bottom: 15%;">
         <div style="font-size: -webkit-xxx-large; text-align: center; color: #ff2222; padding-top: 5%;padding-bottom: 5%;">오늘의 뉴스</div>
         <a href="https://search.naver.com/search.naver?where=nexearch&amp;sm=top_hty&amp;fbm=1&amp;ie=utf8&amp;query=%EB%82%A0%EC%94%A8" style="display: block;text-align: -webkit-center;padding-top: 15px;padding-bottom: 15px; color: black;">오늘의 날씨</a>
     <div style="display: grid;padding: 15px;grid-template-columns: 1fr 1fr 1fr;text-align: center; font-size: 17px;">
         <div class="day_data"> 
             <div class="cell_date">
                 <span class="date_inner">
-                    <strong class="day">{weather_today_daylist[0]}</strong>
+                    <strong class="day">content</strong>
                     <span class="date">{weather_today_numdaylist[0]}</span>
                 </span>
             </div> 
@@ -91,40 +108,12 @@ file.write(f'''<div style="background-color: white;padding-bottom: 15%;">
     </div>
         <table style="padding:15px 0px 15px 0px;border:0;width:100%;height:0;background:none;border-top-width:1px;border-top-style:dashed;border-top-color: red;margin:0 0"></table>
             <div style="margin-left: 5%;margin-right: 5%;padding-bottom: 30px;" class="rank">
-                <ul>
-                    <a href="{asideBox_hrefs[0]}" style="color: black; font-size: initial;">1.  {asideBox_all_titles[0]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[1]}" style="color: black; font-size: initial;">2.  {asideBox_all_titles[1]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[2]}" style="color: black; font-size: initial;">3.  {asideBox_all_titles[2]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[3]}" style="color: black; font-size: initial;">4.  {asideBox_all_titles[3]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[4]}" style="color: black; font-size: initial;">5.  {asideBox_all_titles[4]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[5]}" style="color: black; font-size: initial;">6.  {asideBox_all_titles[5]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[6]}" style="color: black; font-size: initial;">7.  {asideBox_all_titles[6]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[7]}" style="color: black; font-size: initial;">8.  {asideBox_all_titles[7]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[8]}" style="color: black; font-size: initial;">9.  {asideBox_all_titles[8]}</a>
-                </ul>
-                <ul>
-                    <a href="{asideBox_hrefs[9]}" style="color: black; font-size: initial;">10.  {asideBox_all_titles[9]}</a>
-                </ul>
-
+                {asideBox_ul}
             </div>
             <table style="padding:15px 0px 15px 0px;border:0;width:100%;height:0;background:none;border-top-width:1px;border-top-style:dashed;border-top-color: red;margin:0 0"></table>
-        </div>''')
+        </div>'''.format(asideBox_ul=asideBox_ul)
+
+file.write(file_contents)
 file.close()
 
 #메일 보내는 라이브러리 / send_who 에는 이메일 주소를 string 으로 보내준다 
